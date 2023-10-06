412877e02085ae14ac30dd7a00bcf28f07783610
Karthik Desingu <karthikdesingu2000@gmail.com>
2023-10-06 08:24:03 -0400

    cleanup

---
+++ A1_Cellular-Motility-Viscek/assets/submission/DESINGU-KARTHIK_msd.m
@@ -0,0 +1,112 @@
+%{
+Calculate the mean squared displacement (MSD) averaged over all
+particles in the system for different values of the noise; use η = 0.4, 0.5, 0.6, 0.7 and
+0.8, and at fixed packing fraction φ = 0.5. Use a simulation with at least NT = 5 × 104
+steps, and neglect data from the first 2500 steps. Make a plot of MSD vs. the time
+window size ∆t for these different values for the noise. As in assignment 0, calculate
+the diffusion coefficient D and power law α from the plotted data; make a plot of
+D and α as a function of noise η.
+%}
+
+eta_values = (0.4:0.1:0.8)';
+
+%% Set the values of other parameters.
+
+% Number of particles
+N = 100;
+
+% Define Simulation Parameters
+r0      = 2.0;              % Zone of interaction, in units of particle diameters
+v0      = 0.05;             % Speed of cells
+Nsteps  = 50000;             % Number of steps for the simulation
+dt      = 0.005;            % Simulation timestep
+beta    = 10000;            % Value of beta for the simulation
+
+% Number of plot steps to skip between consle output
+Nplot   = Nsteps/25;
+
+% Packing fraction 
+phi = 0.5;
+
+% collect alphas and diffusion coefficients.
+alphas = zeros(size(eta_values, 1), 1);
+diffusionCoefficients = zeros(size(eta_values, 1), 1);
+
+plot_colors = ['r', 'g', 'b', 'm', 'k'];
+
+figure(1), clf, hold on, box on;
+for i = 1:size(eta_values, 1)
+    eta = eta_values(i);
+
+    % run simulation.
+    [xtotal, ytotal, pols] = vicsek(N, phi, r0, v0, dt, eta, beta, Nsteps, Nplot);
+
+    % neglect first 2500 time steps.
+    xtotal = xtotal(:, 2501:end);
+    ytotal = ytotal(:, 2501:end);
+
+    % compute and store MSDs for each particle.
+    MSDtotal = zeros(N, Nsteps-2500-1);
+    for j = 1:N
+        MSDtotal(j, :) = compute_msd(xtotal(j, :)', ytotal(j, :)');
+    end
+
+    % average the MSD across all particle.
+    MSD_average = mean(MSDtotal, 1);
+    deltaT = dt.*(1:size(MSD_average, 2));
+
+    % estimate coefficients of power law.
+    linearCoeffs = polyfit(log10(deltaT), log10(MSD_average), 1);
+    alphas(i) = linearCoeffs(1);
+    diffusionCoefficients(i) = 10^linearCoeffs(2);
+
+    % plot MSD vs. time window, for current noise value.
+	plot(deltaT, MSD_average, "Color", plot_colors(i));
+	hold on;
+end
+
+% make plots verbose.
+hold off;
+xlabel('time window','Interpreter','latex');
+ylabel('average MSD','Interpreter','latex');
+legend('eta=0.4', 'eta=0.5', 'eta=0.6', 'eta=0.7', 'eta=0.8');
+ax = gca;
+ax.FontSize = 18;
+% ax.XScale = 'log';
+% ax.YScale = 'log';
+
+
+% plot coefficients against noise value.
+figure(2);
+plot(eta_values, alphas, "Color", "b");
+hold on;
+plot(eta_values, diffusionCoefficients, "Color", "r");
+xlabel('noise strength $(\eta)$','Interpreter','latex');
+ylabel('$\alpha$ or $D$','Interpreter','latex');
+legend('$\alpha$', '$D$');
+ax = gca;
+ax.FontSize = 18;
+
+
+% helper function to compute the MSD for a given set of X and Y positions for several time points.
+function MSD = compute_msd(xs, ys)
+    % number of time points
+    NT = size(xs, 1);
+    MSD = zeros(NT-1, 1);
+
+    % loop over all possible time window sizes.
+    for tw = 1:(NT-1)
+	
+        %% -- Each loop deals with delT = 1, 2, 3, ... and computes one row of the MSD in the end.
+        
+        % calculate x and y displacements, separated by tw (time window).
+        dxs = xs(1+tw:end) - xs(1:end-tw);
+        dys = ys(1+tw:end) - ys(1:end-tw);
+        
+        % take mean over all displacements.
+        dispMean = mean( dxs.^2 + dys.^2 );
+        
+        % store in MSD array
+        MSD(tw) = dispMean;
+    end
+end
---
+++ A1_Cellular-Motility-Viscek/assets/submission/DESINGU-KARTHIK_polarization.m
@@ -0,0 +1,69 @@
+%{
+In a new script, calculate the time-averaged polarization Φ as a function of noise
+strength η and packing fraction φ; use φ = 0.25,0.375, and 0.5. For the noise strength,
+use values between η = 0.5 and η = 0.7, with points separated by 0.01. Make a plot of
+Φ vs η for the three different values of φ from simulations run for NT = 10000 time steps.
+For your calculation of Φ, only use velocities from the second half of the simulation
+to minimize transient effects that occur at thebeginning of the simulations.
+%}
+
+% clear workspace
+clear;
+
+% close any open figures
+close all;
+
+% clear text from command window
+clc;
+
+% Set ranges for noise and packing fraction, respectively.
+eta_values = (0.5:0.01:0.7)';
+phi_values = [0.25; 0.375; 0.5];
+
+
+%% Set the values of other parameters.
+
+% Number of particles
+N = 100;
+
+% Define Simulation Parameters
+r0      = 2.0;              % Zone of interaction, in units of particle diameters
+v0      = 0.05;             % Speed of cells
+Nsteps  = 10000;             % Number of steps for the simulation
+dt      = 0.005;            % Simulation timestep
+beta    = 10000;            % Value of beta for the simulation
+
+% Number of plot steps to skip between consle output
+Nplot   = Nsteps/25;
+
+plot_colors = ['r', 'g', 'b'];
+
+figure(1), clf, hold on, box on;
+for i=1:size(phi_values, 1)
+    phi = phi_values(i);
+
+	% collect averaged polarization values.
+	avg_pols = zeros(size(eta_values, 1));
+
+	for j=1:size(eta_values, 1)
+		eta = eta_values(j);
+
+		% run simulation
+		[xtotal, ytotal, pols] = vicsek(N,phi,r0,v0,dt,eta,beta,Nsteps,Nplot);
+
+		% collect polarization value.
+		avg_pols(j) = mean(pols(ceil(Nsteps/2):end));
+	end
+
+	% plot pol vs. eta, for current packing fraction.
+	plot(eta_values, avg_pols, "Color", plot_colors(i));
+	hold on;
+end
+
+hold off;
+xlabel('noise strength $(\eta)$','Interpreter','latex');
+ylabel('time-averaged polarization $(<\Phi(t)>)$','Interpreter','latex');
+legend('pf=0.250', 'pf=0.375', 'pf=0.500');
+ax = gca;
+ax.FontSize = 18;
+
