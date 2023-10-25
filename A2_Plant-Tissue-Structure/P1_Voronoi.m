aec6930d1c4220c52c454aecdca9ac1b1daeadac
Karthik Desingu <karthikdesingu2000@gmail.com>
2023-10-25 16:36:07 -0400

    restructure

--- A2_Plant-Tissue-Structure/P1_Voronoi.m
+++ A2_Plant-Tissue-Structure/P1_Voronoi.m
@@ -1,11 +1,15 @@
-N = 50;
+%% (A) Uniform random points.
 
+N = 50;
 randomPosns = rand(2, N);
 
 figure(1);
 [V, C, vAll, cAll, xbox, ybox] = getVoronoiDiagram(randomPosns(1, :), randomPosns(2, :), 1);
 
 % save generated data and plot.
-saveas(gcf, 'voronoi_random.png');
-save('vertexPosns_random.mat', 'V');
-save('cellsVertices_random.mat', 'C');
+saveas(gcf, 'outputs/voronoi_random.fig');
+save('outputs/vertexPosns_random.mat', 'V');
+save('outputs/cellsVertices_random.mat', 'C');
+
+
+%% (B)-1 
