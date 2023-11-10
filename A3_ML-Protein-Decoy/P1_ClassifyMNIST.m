ee73bf3c09454dd727a07b1583eafb103fdcefd6
Karthik Desingu <karthikdesingu2000@gmail.com>
2023-11-10 16:23:04 -0500

    data prep

---
+++ A3_ML-Protein-Decoy/P1_ClassifyMNIST.m
@@ -0,0 +1,30 @@
+% get data.
+[XTrain, YTrain, anglesTrain] = digitTrain4DArrayData;
+[XTest, YTest, anglesTest] = digitTest4DArrayData;
+
+
+% get matrix dimensions and show sample image.
+train_size = size(XTrain);
+test_size = size(XTest);
+imshow(XTrain(:, :, :, 1));
+
+
+% flatten the train and test arrays.
+XTrain = reshape(XTrain, prod(train_size(1:3), "all"), train_size(4));
+XTest = reshape(XTest, prod(test_size(1:3), "all"), test_size(4));
+% update train and test sizes.
+train_size = size(XTrain);
+test_size = size(XTest);
+
+
+% one-hot encode the Y vectors.
+[class_ids, class_names] = grp2idx(YTrain);
+YTrain_OneHot = zeros(numel(class_ids), train_size(2));
+for i = 1:train_size(2)
+	YTrain_OneHot(class_ids(i), i) = 1;
+end
+
+YTest_OneHot = zeros(numel(class_ids), test_size(2));
+for i = 1:test_size(2)
+	YTest_OneHot(class_ids(i), i) = 1;
+end
